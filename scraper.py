55b7581a4d1ce17ec1118ef4cdaefdd511cd4051
César_Mercado <doncesarmercado@hotmail.com>
2025-03-20 09:46:58 +0100

    feat(scraper.py): modified

--- scraper.py
+++ scraper.py
@@ -6,10 +6,11 @@ from mongodb_connection import collection
 URL = "https://www.technologyreview.com/"
 HEADERS = {"User-Agent": "Mozilla/5.0"}
 
+
 # Obtener los artículos de la página
 def get_articles():
     response = requests.get(URL, headers=HEADERS)
-    
+
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
 
@@ -27,10 +28,10 @@ def get_articles():
                     articles.append({"title": title})
 
         return articles
-    
+
     else:
         print(f"Error {response.status_code}: No se pudo acceder a la página {URL}")
-        
+
         return []
 
 
@@ -38,10 +39,9 @@ def get_articles():
 def save_to_mongo(data):
     if data:
         for article in data:
+            # Si el artículo ya existe, lo actualizamos
             collection.update_one(
-                {"title": article["title"]},
-                {"$set": article},
-                upsert=True
+                {"title": article["title"]}, {"$set": article}, upsert=True
             )
         print(f"{len(data)} artículos guardados en MongoDB.")
 
@@ -50,4 +50,3 @@ def save_to_mongo(data):
 if __name__ == "__main__":
     articles = get_articles()
     save_to_mongo(articles)
-    
