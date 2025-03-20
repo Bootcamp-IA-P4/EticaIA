e5ede6ad5315e2727a9da035f4ab436e650a94fd
César_Mercado <doncesarmercado@hotmail.com>
2025-03-20 22:39:31 +0100

    fix(scraper.py): asynchrony errors with asyncio import and changes in save_to_mongo function call

--- scraper.py
+++ scraper.py
@@ -1,52 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
 from api.database import collection
+import asyncio
 
 URL = "https://www.technologyreview.com/"
 HEADERS = {"User-Agent": "Mozilla/5.0"}
 
+
 def get_articles():
     response = requests.get(URL, headers=HEADERS)
-    
+
     if response.status_code != 200:
         print(f" Error {response.status_code}: No se pudo acceder a la página.")
         return []
-    
+
     soup = BeautifulSoup(response.text, "html.parser")
 
     articles = []
 
     for article_tag in soup.find_all("a", href=True):
         title_tag = article_tag.find("h3")
-        topic_tag = article_tag.find_previous("span", class_="homepageEyebrow__text--e755f0749c2ee2902850fa01b4150520")  
+        topic_tag = article_tag.find_previous(
+            "span", class_="homepageEyebrow__text--e755f0749c2ee2902850fa01b4150520"
+        )
         topic = topic_tag.text.strip() if topic_tag else "Unknown"
 
         link = article_tag["href"]
-        if not link.startswith("http"): 
+        if not link.startswith("http"):
             link = f"https://www.technologyreview.com{link}"
 
         if title_tag and title_tag.text.strip():  # Verifica que haya título válido
-            articles.append({
-                "title": title_tag.text.strip(),
-                "link": link,
-                "topic": topic  
-            })
+            articles.append(
+                {"title": title_tag.text.strip(), "link": link, "topic": topic}
+            )
         else:
             print(f"⚠️ Artículo ignorado por no tener título: {link}")
 
-    return articles  
+    return articles
 
-def save_to_mongo(data):
+
+async def save_to_mongo(data):
     """Guarda o actualiza los artículos en MongoDB."""
     saved_count = 0
+    updated_count = 0
+
     for article in data:
-        result = collection.update_one(
+        result = await collection.update_one(
             {"title": article["title"]},  # Si existe el mismo título, actualiza
             {"$set": article},
-            upsert=True
+            upsert=True,
         )
-        
-        if result.upserted_id:  
+
+        if result.matched_count == 0:
             print(f"✅ Guardado: {article['title']}")
             saved_count += 1
         else:
@@ -54,8 +59,9 @@ def save_to_mongo(data):
 
     print(f"🗃️ Total de artículos guardados o actualizados: {saved_count}")
 
+
 if __name__ == "__main__":
     articles = get_articles()
     print(f"📥 Artículos extraídos: {len(articles)}")
-    save_to_mongo(articles)
+    asyncio.run(save_to_mongo(articles))
     print("🆗Proceso completado.🆗")
