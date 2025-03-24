b96052d476450a2a3a3e3dc2b7e99192ee5dc480
César_Mercado <doncesarmercado@hotmail.com>
2025-03-24 09:48:49 +0100

    fea(App.jsx): Rendering actual articles from the API on the main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ArticleDetail from './pages/ArticleDetail';
 import Navbar from './components/Navbar';
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <>
-      <Navbar />
+      <Navbar onSearch={setSearchTerm} />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home searchTerm={searchTerm} />} />
         <Route path="/article/:id" element={<ArticleDetail />} />
       </Routes>
     </>
